373cdd895454fe37ab2da279691f38a033a67863
cbretheim <33535137+cbretheim@users.noreply.github.com>
2019-04-07 22:38:11 +0200

    Add files via upload

---
+++ wk10/script.js
@@ -0,0 +1,33 @@
+
+var myVue = new Vue({
+ el: "#app",
+ data: {
+   consoles: [
+     { name: "Elbow Chair",
+       model: "CH20",
+       year: 1956,
+       materials: "oak and leather",
+       height: 73},
+     { name: "Dining chair",
+       model: "CH23",
+       year: 1950,
+       materials: "oak, walnut, natural cord",
+       height: 78.6},
+     { name: "Wishbone Chair",
+       model: "CH24",
+       year: 1949,
+       materials: "oak, natural cord",
+       height: 76 },
+     { name: "Lounge Chair",
+       model: "CH22",
+       year: 1950,
+       materials: "oak, walnut, natural cord",
+       height: 72.6},
+     { name: "Shell Chair",
+       model: "CH07",
+       year: 1963,
+       materials: "oak and leather",
+       height: 74 }
+   ]
+ }
+});
